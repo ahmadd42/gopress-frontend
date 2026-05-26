0816acb9ff781018c7c9821ecd990784ac07ce52
Ahmad Rasheed <ahmadd42@gmail.com>
2026-05-26 18:32:50 +0500

    Changed directory structure

---
+++ viewer.js
@@ -0,0 +1,50 @@
+/*export async function onRequestGet(context) {
+
+  return new Response(`
+    <html>
+      <head>
+        <title>TEST TITLE</title>
+      </head>
+      <body>
+        HELLO
+      </body>
+    </html>
+  `, {
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}*/
+
+export default {
+
+  async fetch(request, env) {
+
+    const url = new URL(request.url);
+
+    // Dynamic route
+    if (url.pathname === "/view") {
+
+      return new Response(`
+        <html>
+          <head>
+            <title>FUNCTION WORKING</title>
+          </head>
+          <body>
+            Worker route works
+          </body>
+        </html>
+      `, {
+        headers: {
+          "content-type": "text/html"
+        }
+      });
+
+    }
+
+    // Serve normal static files
+    return env.ASSETS.fetch(request);
+
+  }
+
+}
